0343ab60c7597498c04f9cfbccf4c3adcb7624d2
Andrew VanNess <avanness@student.bridgew.edu>
2018-10-21 01:05:59 -0400

    add basic functionality and UI, user can enter note and upload to DB, user then taken to shareable link with note

--- app.py
+++ app.py
@@ -3,24 +3,29 @@ from json import dumps
 from string import Template
 from collections import Counter
 from datetime import datetime
+import psycopg2
+import os 
+import random
+import string
 
 app = Flask(__name__)
 
+DATABASE_URL = os.environ['DATABASE_URL']
 
 HTML_TEMPLATE = Template("""
-<h1>Hello ${note_ID}!</h1>
+<h1>${note_ID}</h1>
 
-<p>Hello world!</p>
+			<textarea id="textbox" rows="4" cols="50">
+			${content}
+			</textarea>
 """)
 
 
-@app.route('/places/<p>')
-def places_view(p):
-    return(HTML_TEMPLATE.substitute(note_ID=p))
-
 @app.route('/notes/<url_note_ID>')
 def note_page(url_note_ID):
-    return(HTML_TEMPLATE.substitute(note_ID=url_note_ID))
+    foundContent = fetchFromTable(url_note_ID)
+
+    return(HTML_TEMPLATE.substitute(note_ID=url_note_ID, content= foundContent))
 
 
     """
@@ -30,6 +35,15 @@ def note_page(url_note_ID):
     """
 
 
+@app.route('/post/', methods=['POST'])
+def poast():
+    print(request.form.get('text', 0))    
+    messageText = request.form.get('text', 0)
+    generatedID = ''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(9))
+
+
+    print(addToTable(messageText, generatedID))
+    return(generatedID)
 
 
 @app.route('/')
@@ -37,28 +51,86 @@ def index():
     return render_template('index.html')
 
 
-@app.route('/notes/', methods=['POST'])
-def poast():
-    print(request.form.get('letters', 0))
-    # letters = list(request.form.get('letters', 0).lower())
-    # charSet = Counter(letters)
 
-    # matches = []
-    # with open('scrabble.txt', 'r') as f:
-    #     input = f.readlines()
 
-    # # traverse words in list, remove \n then check if the word can be made from the letters given. return an array of matched words.
-    # for word in input:
-    #     word = word.replace('\n', '').lower()
-    #     # if first letter isn't in the character list, skip it
-    #     if word[0] not in charSet:
-    #         continue
+def fetchFromTable(postID):
 
-    #     if not Counter(word) - charSet:
-    #         matches.append(word)
+    conn = None
+
+    try:
+
+        conn = psycopg2.connect(DATABASE_URL, sslmode='require')
+        cur = conn.cursor()
+        #cur.execute('SELECT FROM poasts WHERE ID=' + postID)
+        cur.execute('SELECT CONTENT FROM poasts WHERE ID = ' + "'" + postID + "'")
+        row = cur.fetchone()
         
+        print(row)
 
-    # return make_response(dumps(matches))
+        # close communication with the PostgreSQL database server
+        cur.close()
+        # commit the changes
+        conn.commit()
+
+        print(row)
+        return row
+
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(error)
+        return error
+
+    finally:
+        if conn is not None:
+            conn.close()
+
+
+
+def addToTable(messageText, postID):
+    #print("----IN HIGHLIGHT FUNCTION----")
+    #print(tableName)
+    #print(messageText)
+
+    conn = None
+    try:
+
+        """ Connect to the PostgreSQL database server """
+        conn = psycopg2.connect(DATABASE_URL, sslmode='require')
+        cur = conn.cursor()
+        # execute a statement
+        # cur.execute(command)
+
+        # working insert
+        #insertcommand = "INSERT INTO poasts " + "(LINK) VALUES ('" + messageText + "');"
+        
+        insertcommand = "INSERT INTO poasts (ID, CONTENT, TITLE, DELETED, DATE_UPLOADED, VIEWS) VALUES ('" + postID + "', " + "'" + messageText + "'" + ", 'test title', null, '2018-10-18 22:20:46', '1');"
+        print(insertcommand)
+        cur.execute(insertcommand)
+
+        cur.close()
+        conn.commit()
+        print("Success. Added " + messageText + " to highlights")
+        return "Added!"
+
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(error)
+        # return error
+
+    finally:
+        if conn is not None:
+            conn.close()
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 if __name__ == '__main__':
