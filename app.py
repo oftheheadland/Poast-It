27dba3f9b7a2408d67750f99bad017f25f836127
Andrew VanNess <avanness@student.bridgew.edu>
2018-10-25 23:40:07 -0400

    add 404 page

--- app.py
+++ app.py
@@ -103,6 +103,12 @@ def index():
     return render_template('index.html')
 
 
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+
+
 def fetchFromTable(postID):
     conn = None
     try:
