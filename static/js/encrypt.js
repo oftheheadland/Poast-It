ad0f46ba30726cfeb11f8c0dc8dd7b6755245d63
Andrew VanNess <avanness@student.bridgew.edu>
2018-11-03 01:02:20 -0400

    minify jquery library and remove unused styles

--- static/js/encrypt.js
+++ static/js/encrypt.js
@@ -1,6 +1,6 @@
 function makeid() {
     var text = "";
-    var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()_+-=[]\;',./{}|:<>?~`";
+    var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()_+-=[];',./{}|:<>?~`";
   
     for (var i = 0; i < 30; i++) {
       text += possible.charAt(Math.floor(Math.random() * possible.length));
